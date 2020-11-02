f5526c6a9277789fc9aad048a4886ce1257bf528
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 22:43:01 +0600

    week1_lecture4

---
+++ Lecture4.py
@@ -0,0 +1,23 @@
+int = 5
+print(int)
+num1  = 1
+
+
+# print(num1)
+# number = input("right the number")
+# print(number)
+# print(type(num1))
+# print(type(number))
+
+# * # umnojeniye
+# a = 20
+# b = 3
+# print(a>b)
+# print(a==b)
+number = 25
+number2 = 5
+
+# number = number - number2
+# number -= number2
+number *= number2
+print(number)
